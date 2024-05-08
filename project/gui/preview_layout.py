60819205a21e9aaa7dde8eeb93aaa7c2baef8bcc
mik2003 <62058516+mik2003@users.noreply.github.com>
2024-05-08 22:44:15 +0200

    Try shapes in gui scene

--- project/gui/preview_layout.py
+++ project/gui/preview_layout.py
@@ -1,6 +1,15 @@
 """Module for the layout of the drawing preview."""
 
-from PySide6.QtWidgets import QGraphicsScene, QGraphicsView, QVBoxLayout
+from PySide6.QtCore import QPointF
+from PySide6.QtGui import QPolygonF
+from PySide6.QtWidgets import (
+    QGraphicsPolygonItem,
+    QGraphicsScene,
+    QGraphicsView,
+    QVBoxLayout,
+)
+
+from project.core.geometry import n_polygon
 
 
 class PreviewLayout(QVBoxLayout):
@@ -11,6 +20,13 @@ class PreviewLayout(QVBoxLayout):
 
         self.drawing_scene = QGraphicsScene()
         self.drawing_scene.addText("Hello!")
+        self.draw_polygon()
         self.drawing_view = QGraphicsView(self.drawing_scene)
 
         self.addWidget(self.drawing_view)
+
+    def draw_polygon(self) -> None:
+        vertices = 100 * n_polygon(13)
+        polygon = QPolygonF([QPointF(*p) for p in vertices[0:2, :].T])
+        polygon_item = QGraphicsPolygonItem(polygon)
+        self.drawing_scene.addItem(polygon_item)
