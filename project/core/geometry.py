727a9bbfd8f834c1bcd5f93e19657e1e4b0a1ee4
mik2003 <62058516+mik2003@users.noreply.github.com>
2024-05-09 23:56:46 +0200

    Implement spirograph curve

--- project/core/geometry.py
+++ project/core/geometry.py
@@ -1,3 +1,4 @@
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
@@ -12,11 +13,63 @@ def n_polygon(n: int) -> NDArray:
     ----------
     n : int
         Number of sides.
+
+    Returns
+    -------
+    NDArray : Polygon vertices of shape (3xn) since
+    homogeneous coordinates are used.
     """
     angles = np.linspace(0, 2 * np.pi, n + 1)
-    out = np.stack([np.cos(angles), np.sin(angles), np.ones_like(angles)])
+    return np.stack([np.cos(angles), np.sin(angles), np.ones_like(angles)])
 
-    return out
+
+class Spirograph:
+
+    @staticmethod
+    def trajectory(l_r: float, k_r: float, t: NDArray) -> NDArray:
+        """
+        Calculate spirograph trajectory
+
+        Parameters
+        ----------
+        l_r : float
+            Ratio between drawing point on smaller circle and radius of
+            smaller circle (rho/r), physically must be smaller than 1.
+        k_r : float
+            Ratio between radius of small circle
+            to stationary circle (r/R, R=1).
+        t : NDArray
+            Discretization of angle t.
+
+        Returns
+        -------
+        NDArray : Spirograph trajectory of shape (3xn) since
+        homogeneous coordinates are used.
+        """
+        xp = (1 - k_r) * np.cos(t) + l_r * k_r * np.cos((1 - k_r) / k_r * t)
+        yp = (1 - k_r) * np.sin(t) - l_r * k_r * np.sin((1 - k_r) / k_r * t)
+        return np.stack([xp, yp, np.ones_like(t)])
+
+    @staticmethod
+    def angles(ti: float, tf: float, steps: int) -> NDArray:
+        """
+        Calculate spirograph angles discretization.
+
+        Parameters
+        ----------
+        ti : float
+            Initial angle t (between segment connecting
+            the two circles centers and x-axis).
+        tf : float
+            Final angle t.
+        steps : int
+            Number of steps in the angle t discretization.
+
+        Returns
+        -------
+        NDArray : Discretization of angle t.
+        """
+        return np.linspace(ti, tf, steps)
 
 
 class Transform:
@@ -266,11 +319,52 @@ class Transform:
 
 
 if __name__ == "__main__":
-    polygon = n_polygon(3)
+    # polygon = n_polygon(3)
     # polygon = Transform.rotate(polygon, np.pi / 4)
     # polygon = Transform.translate(polygon, 1, 2)
-    polygon = Transform(polygon).rotate_p(np.pi / 4, 1, 0).a
-    print(polygon)
-    plt.plot(polygon[0, :], polygon[1, :])
-    plt.gca().set_aspect("equal", adjustable="box")
+    # polygon = Transform(polygon).rotate_p(np.pi / 4, 1, 0).a
+    # print(polygon)
+    # plt.plot(polygon[0, :], polygon[1, :])
+
+    l_r = 0.8
+    k_r = 0.67
+
+    steps = 1000
+
+    t = Spirograph.angles(0, 2 * np.pi, steps)
+    spiro = Spirograph.trajectory(l_r, k_r, t)
+    # plt.plot(spiro_x, spiro_y)
+
+    # plt.gca().set_aspect("equal", adjustable="box")
+    # plt.show()
+
+    c_x = (1 - k_r) * np.cos(t)
+    c_y = (1 - k_r) * np.sin(t)
+
+    c1 = plt.Circle((0, 0), 1, fill=False)
+    c2 = plt.Circle((c_x[0], c_y[0]), k_r, fill=False)
+
+    fig, ax = plt.subplots()
+    ax.add_patch(c1)
+    ax.add_patch(c2)
+    dl = ax.plot([c_x[0], spiro[0, 0]], [c_y[0], spiro[1, 0]], color="black")[
+        0
+    ]
+    line = ax.plot(spiro[0, 0], spiro[1, 0])[0]
+    ax.set(xlim=[-1, 1], ylim=[-1, 1])
+
+    fig.gca().set_aspect("equal", adjustable="box")
+
+    def update(frame):
+        c2.set_center((c_x[frame], c_y[frame]))
+        dl.set_xdata([c_x[frame], spiro[0, frame]])
+        dl.set_ydata([c_y[frame], spiro[1, frame]])
+        # update the line plot:
+        line.set_xdata(spiro[0, :frame])
+        line.set_ydata(spiro[1, :frame])
+        return line
+
+    ani = animation.FuncAnimation(
+        fig=fig, func=update, frames=steps, interval=10
+    )
     plt.show()
